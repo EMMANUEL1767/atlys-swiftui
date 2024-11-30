7d599642494d4fa98bbdea18e8b6c740b5366a54
Emmanuel Biju <emmanuelbiju4@gmail.com>
2024-11-30 13:22:33 +0530

    Initial Commit

---
+++ atlys-swiftui/atlys_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  atlys_swiftuiApp.swift
+//  atlys-swiftui
+//
+//  Created by Emmanuel Biju on 30/11/24.
+//
+
+import SwiftUI
+
+@main
+struct atlys_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
